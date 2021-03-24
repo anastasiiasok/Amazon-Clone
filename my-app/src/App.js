8dcd16b09efa799d36a4e443c8f320fa4705c607
Anastasiia Soktoeva <anastasiiasoktoeva@anastasiias-air.myfiosgateway.com>
2021-03-24 19:17:55 -0400

    Part 1: created components (Header, Home and Checkout), Header CSS, installed Material-UI icons and created Routes and Links

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,28 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Components/Checkout";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
-      <p> Amazon Clone!</p>
+      <Router>
+        <div className="app">
+          <Header />
+
+          <Switch>
+            <Route path="/checkout">
+              <Checkout />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
