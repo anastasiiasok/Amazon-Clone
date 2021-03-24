d695dcc158ba9f877061167c6d54dfb5eb5c0d42
Anastasiia Soktoeva <anastasiiasoktoeva@anastasiias-air.myfiosgateway.com>
2021-03-24 19:26:58 -0400

    Created Home.css and added image to the home page

--- my-app/src/Components/Home.jsx
+++ my-app/src/Components/Home.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import "./Home.css";
 
 function Home() {
   return (
-    <>
-      <h1>Home page</h1>
-    </>
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+          alt=""
+        />
+      </div>
+    </div>
   );
 }
 
