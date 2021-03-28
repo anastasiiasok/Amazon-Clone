69afd47b665698f5dd96f78beaf64a31c77fa966
Anastasiia Soktoeva <anastasiiasoktoeva@anastasiias-air.myfiosgateway.com>
2021-03-28 15:12:36 -0400

    created DataBase with products and images

---
+++ my-app/src/Components/Data/Products/index.js
@@ -0,0 +1,29 @@
+import id1 from "../../../Assets/Images/id1.jpg";
+import id2 from "../../../Assets/Images/id2.jpg";
+
+export const Products = [
+  {
+    title: "Harness Puppy Padded Pet Harnesses for Cat Small Dogs",
+    image: id1,
+    rating: "5",
+    ratings: "2,422",
+    price: "$12.99",
+    salePrice: "12.00",
+    isPrime: true,
+    displaySize: "Chest 14.75-15.35",
+    brand: "PUPTECK",
+  },
+  {
+    title:
+      "One Hand Gaming Keyboard and Mouse Combo, 39 Keys PUBG Keycap Version",
+    image: id2,
+    rating: "5",
+    ratings: "1,036",
+    price: "$25.00",
+    salePrice: "23.99",
+    isPrime: false,
+    numberofButtons: "35",
+    connectivity: "Wi-Fi, Bluetooth",
+    brand: "EQEOVGA",
+  },
+];
