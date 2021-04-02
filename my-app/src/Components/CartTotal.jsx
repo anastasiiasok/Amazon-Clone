ddacc69bb7764839cd6e3ecc2a04ff45f7ecc36e
Anastasiia Soktoeva <anastasiiasoktoeva@anastasiias-air.myfiosgateway.com>
2021-04-02 15:13:58 -0400

    Created and styled CartTotal component

--- my-app/src/Components/CartTotal.jsx
+++ my-app/src/Components/CartTotal.jsx
@@ -1,11 +1,75 @@
 import React from "react";
+import styled from "styled-components";
 
 function CartTotal() {
   return (
     <div>
-      <p>This is the total</p>
+      <Container>
+        <TotalContainer>
+          <Title>Subtotal $</Title>
+          <PriceContainer></PriceContainer>
+        </TotalContainer>
+        <GiftContainer>
+          <Checkbox>
+            <input type="checkbox" />
+          </Checkbox>
+          <GiftText>This order contains a gift</GiftText>
+        </GiftContainer>
+        <Button>Proceed to checkout</Button>
+      </Container>
     </div>
   );
 }
 
 export default CartTotal;
+
+const Container = styled.div`
+  height: 120px;
+  background-color: white;
+  margin-right: 18px;
+  padding: 20px;
+
+  flex-direction: column;
+`;
+
+const TotalContainer = styled.div`
+  display: flex;
+`;
+
+const GiftContainer = styled.div`
+  display: flex;
+`;
+
+const Checkbox = styled.div``;
+
+const GiftText = styled.div`
+  margin-left: 8px;
+`;
+
+const Title = styled.div`
+  margin-bottom: 8px;
+  font-size: 20px;
+`;
+
+const PriceContainer = styled.div`
+  margin-left: 8px;
+  font-weight: 700;
+  font-size: 20px;
+`;
+
+const Button = styled.button`
+  box-shadow: 0 2px 5px 0 rgb(213 217 217 / 50%);
+  background: #ffd814;
+  background-color: #fcd200;
+  padding: 10px;
+  width: 100%;
+  margin: 20px 0px;
+  border-radius: 8px;
+  border: none;
+  cursor: pointer;
+  :hover {
+    background: #f7ca00;
+    border-color: #f2c200;
+    box-shadow: 0 2px 5px 0 rgb(213 217 217 / 50%);
+  }
+`;
